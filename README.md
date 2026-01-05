3a46341d50746ea7633e5cb2581d94b2c3598f3d
marcosfantonio <devfantonio@gmail.com>
2026-01-05 14:41:12 -0300

    Initial commit: 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# 🌌 Nebula Theme for Archcraft
+
+My Custom theme for Archcraft that make use of minimalistic yet pastel/cyberpunk elements.
+
+Meu tema customizado que uso no Archcraft, focado em ser minimalista porem com elementos pasteis/cyberpunk
+
+## 📸 Preview
+![Screenshot](screenshot.png)
+
+## 🔧 Componentes
+- **WM:** Openbox
+- **Bar:** Polybar
+- **Dock:** Plank (Nebula Theme)
+- **Terminal:** Alacritty + Starship
+- **Compositor:** Picom (Dual Kawase Blur)
+- **Icons:** Tela-circle-dracula
+- **Cursor:** Bibata-Modern-Ice
---
+++ openbox/themes/nebula/theme.bash
@@ -0,0 +1,98 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2025 Marcos Fantonio <devfantonio@gmail.com>
+#
+# Nebula Teme
+# ------------------------------------------------------------------------------
+
+# Colors - Astro-Pop
+
+background='#2E2B5F'
+
+foreground='#AEEEEE'
+
+color0='#2E2B5F'     
+
+color1='#FF7CBF'
+color2='#00FFFF'
+color3='#FFD166'
+color4='#6A5ACD'
+color5='#C77DFF'
+color6='#80F2F2'
+color7='#E0E0E0'
+
+color8='#4B4496'
+color9='#FF9ECE'
+color10='#50FFFF'
+color11='#FFE08A'
+color12='#8476E6'
+color13='#DCA3FF'
+color14='#AEEEEE'
+color15='#FFFFFF'
+
+accent='#00FFFF'
+
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='MesloLGS NF:size=10;3'
+
+# Rofi
+rofi_font='Inter 10'
+rofi_icon='Luv-Folders-Dark'
+
+# Terminal
+terminal_font_name='MesloLGS NF'
+terminal_font_size='10'
+
+# Geany
+geany_colors='manhattan.conf'
+geany_font='MesloLGS NF 10'
+
+# Appearance
+gtk_font='Inter 10'
+gtk_theme='Catppuccin-Mocha-Standard-Blue-Dark'
+icon_theme='Tela-circle-purple-dark'
+cursor_theme='Bibata-Modern-Classic'
+
+# Openbox
+ob_theme='catppuccin-mocha'
+ob_layout='NLIMC'
+ob_font='Inter'
+ob_font_size='10'
+ob_menu='menu-minimal.xml'
+ob_margin_t='48'
+ob_margin_b='10'
+ob_margin_l='10'
+ob_margin_r='10'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x48'
+dunst_origin='top-right'
+dunst_font='Fira Code 10'
+dunst_border='2'
+dunst_separator='2'
+
+# Plank
+plank_hmode='intelligent'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Nebula'
+plank_icon_size='36'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='12'
+picom_shadow_r='20'
+picom_shadow_o='0.60'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method='dual_kawase'
+picom_blur_strength='6'
